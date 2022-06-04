93bacc27d9d853199e64141a11ec6b3f09f7456b
BakanYusufSina <BakanYusufSina@users.noreply.github.com>
2022-06-04 16:02:17 +0300

    odev2 callback

--- post.js
+++ post.js
@@ -6,7 +6,21 @@ const addPost = ({ post, author }, callback) => {
 }
 
 const getPosts = () => {
-    console.log(posts)
+    try {
+        if (posts.length > 0)
+            posts.map((el, i) => {
+                console.log(i+1, " author :", el.author, " post:", el.post)
+            })
+        else
+            console.log('Post bulunamadı')
+    }
+
+    catch {
+        console.log('Bir hata oluştu!!!')
+    }
+
+
+
 }
 getPosts()
 addPost({ author: 'Yusuf', post: 'deneme' }, getPosts)
